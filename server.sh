e4c5247993dcd76549ac759b975884c205151892
Exegete <exegete@mac.com>
2016-07-01 15:15:13 +0000

    Added environment variables for quick startup.

--- server.sh
+++ server.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# TODO: Remove these once Cloud9 fixes environment bugs.
+export GEM_HOME=/usr/local/rvm/gems/ruby-2.3.0
+export GEM_PATH=/usr/local/rvm/gems/ruby-2.3.0:/usr/local/rvm/gems/ruby-2.3.0@global
+
 bundle check
 
 bundle exec jekyll server --host $IP --port $PORT --baseurl ''
